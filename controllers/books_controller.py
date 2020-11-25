00377667e85f68b76ed10971a2e79b09bdfbff72
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2020-11-25 18:53:15 +0000

    extension tasks done

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Blueprint, redirect
+from flask import Flask, render_template, Blueprint, redirect, request
 from models.book import Book
 import repositories.book_repository as book_repository
 import repositories.author_repository as author_repository
@@ -15,15 +15,33 @@ def books():
 
 # NEW
 # GET '/books/new'
+@books_blueprint.route("/books/new", methods=["GET"])
+def new_book():
+    authors = author_repository.select_all()
+    return render_template("books/new.html", all_authors=authors)
 
 
 # CREATE
 # POST '/books'
+@books_blueprint.route("/books", methods=["POST"])
+def create_book():
+    title = request.form['title']
+    genre = request.form['genre']
+    publisher = request.form['publisher']
+    author_id = request.form['author_id']
+    author = author_repository.select(author_id)
+    book = Book(title, genre, publisher, author)
+    book_repository.save(book)
+
+    return redirect('/books')
 
 
 # SHOW
 # GET '/books/<id>'
-
+@books_blueprint.route('/books/<id>')
+def show_book(id):
+    book = book_repository.select(id)
+    return render_template("books/show.html", book=book)
 
 # EDIT
 # GET '/books/<id>/edit'
